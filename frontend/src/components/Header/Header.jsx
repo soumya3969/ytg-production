e358d43023c38100801b3b17f1da9d8825fe5528
soumya <117825720+soumya3969@users.noreply.github.com>
2023-04-27 14:37:19 +0000

    layout component added

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Footer =() =>{
-    return <div>Footer</div>
+const Header =() =>{
+    return <div>Header</div>
 };
 
-export default Footer;
+export default Header;
