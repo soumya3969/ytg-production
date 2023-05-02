553b6a06eba8cdbb619728358e41dead483fc779
soumya <117825720+soumya3969@users.noreply.github.com>
2023-05-02 15:47:51 +0000

    header update

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Component,Row,Button} from 'reactstrap'
+import {NavLink,Link} from 'react-router-dom'
+
+const nav__links = [
+    {
+        path:'/home',
+        display:'Home'
+    },
+    {
+        path:'#',
+        display:'About'
+    },
+    {
+        path:'/tours',
+        display:'Tours'
+    },
+]
 
 const Header =() =>{
-    return <div>Header</div>
+    return <div>Header</div>;
 };
 
 export default Header;
