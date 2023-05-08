f1fa8571ea787b86e2a935019e58b205624612b9
soumya <117825720+soumya3969@users.noreply.github.com>
2023-05-08 19:34:34 +0000

    Header fixed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Component, Row, Button } from 'reactstrap'
+import { Container, Row, Button } from 'reactstrap'
 import { NavLink, Link } from 'react-router-dom'
 
-import logo from '../../assets/images/logo.png';
-
+import logo from '../../assets/images/logo.png'
+import './header.css'
 
 const nav__links = [
     {
@@ -17,50 +17,50 @@ const nav__links = [
     {
         path: '/tours',
         display: 'Tours'
-    },
+    }
 ]
 
 const Header = () => {
-    return <header className="header">
-        <Container>
-            <Row>
-                <div className="nav__wrapper d-flex align-items-center justify-content-between">
+    return (
+        <header className="header">
+            <Container>
+                <Row>
+                    <div className="nav__wrapper d-flex align-items-center justify-content-between">
 
-                    {/* ======= logo ====== */}
-                    <div className="logo">
-                        <img src={logo} alt="" />
-                    </div>
-                    {/* ======= logo end ====== */}
-                    {/* ======= Menu Start ====== */}
-                    <div className="navigation">
-                        <ul className="menu d-flex align-items-center gap-5">
-                            {
-                                nav__links.map((item, index) => (
+                        {/* =====Logo section ====== */}
+
+                        <div className="logo">
+                            <img src={logo} alt="" />
+                        </div>
+                        {/* =====Logo section end ====== */}
+
+                        {/* =====Menu section start ====== */}
+                        <div className="navigation">
+                            <ul className="menu d-flex align-items-center gap-5">
+                                {nav__links.map((item, index) => (
                                     <li className="nav__item" key={index}>
                                         <NavLink to={item.path}>{item.display}</NavLink>
                                     </li>
-                                ))
-                            }
-                        </ul>
-                    </div>
-
-                    {/* ======= Menu End ====== */}
-
-                    <div className="nav__right d-flex align-items-center gap-4 "  >
-                        <div className="nav__btns d-flex align-items-center gap-4">
-                            <Button className="btn secondary__btn"><link to='/login'>Login</link></Button>
-                            <Button className="btn primary__btn"><link to='/register'>Register</link></Button>
-
+                                ))}
+                            </ul>
                         </div>
+                        {/* =====Menu section end ====== */}
 
-                        <span className="mobile__menu">
-                        <i class="ri-menu-line"></i>
-                        </span>
+                        <div className="nav__right d-flex align-items-center gap-4">
+                            <div className="nab__btns d-flex align-items-center gap-4">
+                                <Button className="btn secondary__btn"><Link to='/login'>Login</Link></Button>
+                                <Button className="btn primary__btn"><Link to='/register'>Register</Link></Button>
+                            </div>
+
+                            <span className="mobile__menu">
+                                <i class="ri-menu-line"></i>
+                            </span>
+                        </div>
                     </div>
-                </div>
-            </Row>
-        </Container>
-    </header>
+                </Row>
+            </Container>
+        </header>
+    );
 };
 
 export default Header;
