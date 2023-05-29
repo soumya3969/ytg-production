7a15e91397f5fe1eb5924c02b8e7979d6d99a200
soumya <117825720+soumya3969@users.noreply.github.com>
2023-05-29 19:43:39 +0000

    tourcard updated

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -22,8 +22,17 @@ const TourCard = ({tour}) => {
           <i class="ri-map-pin-line"></i> {city}
           </span>
           <span className='tour__rating d-flex align-items-center gap-1'>
-          <i class="ri-star-fill"></i> {avgRating} span
+          <i class="ri-star-fill"></i> {avgRating} <span>{reviews.length}</span>
           </span>
+        </div>
+        <h5 className="tour__title"><Link to ={`/tours/${id}`}>{title}</Link></h5>
+        <div className="card__bottom d-flex align-items-center justify-content-between mt-3">
+          <h5>${price} <span> /per person</span></h5>
+
+          <button className="btn booking__btn">
+            <Link to ={`/tours/${id}`}>Book Now</Link>
+          </button>
+
         </div>
       </CardBody>
     </div>
