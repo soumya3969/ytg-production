38fb86c96ca5df124e3127a5b0f786870654408b
soumya <117825720+soumya3969@users.noreply.github.com>
2023-05-29 10:51:23 +0000

    home css updated

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -55,12 +55,30 @@ const Home =()=>{
             <Row>
                 <Col lg='3'>
                     <h5 className="services__subtitle">What we Serve</h5>
-                    <h2 className="services__title">We offer our best services</h2>
+                    <h2 className="services__title">We Offer Our Best Services</h2>
                 </Col>
                 <ServiceList />
             </Row>
         </Container>
     </section>
+
+
+    {/*====== Featured tour section start=======   */}
+    
+    <section>
+        <Container>
+            <Row>
+                <Col lg='12' className="mb-5">
+                    <Subtitle subtitle={'Explore'}/>
+                    <h2 className="featured__tour-title">Our Featured Tours</h2>
+                </Col>
+            </Row>
+        </Container>
+    </section>
+    
+    
+    {/*====== Featured tour section End=======   */}
+    
     </>;
 };
 export default Home;
