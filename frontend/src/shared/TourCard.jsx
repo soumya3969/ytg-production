aaa9082677bdd54b5c7d96faf1bdd80adb8b7172
soumya <117825720+soumya3969@users.noreply.github.com>
2023-05-31 18:46:18 +0000

    tour card css updated

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Card, CardBody} from "reactstrap";
 import {Link} from "react-router-dom";
 
+import './tour-card.css'
+
 const TourCard = ({tour}) => {
   
   const {id, title, city, photo, price, featured, avgRating, reviews } = tour
@@ -9,7 +11,7 @@ const TourCard = ({tour}) => {
   return (
     <div className='tour__card'>
       <Card>
-        <div className="tour__">
+        <div className="tour__img">
           <img src={photo} alt="tour-img" />
           <span>Featured</span>
         </div>
@@ -22,7 +24,7 @@ const TourCard = ({tour}) => {
           <i class="ri-map-pin-line"></i> {city}
           </span>
           <span className='tour__rating d-flex align-items-center gap-1'>
-          <i class="ri-star-fill"></i> {avgRating} <span>{reviews.length}</span>
+          <i class="ri-star-fill"></i> {avgRating} <span>({reviews.length})</span>
           </span>
         </div>
         <h5 className="tour__title"><Link to ={`/tours/${id}`}>{title}</Link></h5>
