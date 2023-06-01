dfafd62ee90938c6f63d0e2c6b74447f27987cd3
soumya <117825720+soumya3969@users.noreply.github.com>
2023-06-01 07:52:29 +0000

    utils and avg rating js added

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import { Card, CardBody } from "reactstrap";
 import { Link } from "react-router-dom";
+import calculateAvgRating from '../utils/avgRating';
 
 import './tour-card.css'
 
 const TourCard = ({ tour }) => {
 
-  const { id, title, city, photo, price, featured, reviews } = tour
+  const { id, title, city, photo, price, featured, reviews } = tour;
 
-  const totalRating = reviews?.reduce((acc, item) => acc + item.rating, 0)
-  const avgRating =
-    totalRating === 0
-      ? ''
-      : totalRating === 1
-        ? totalRating
-        : totalRating / reviews?.length;
+  const {totalRating,avgRating}= calculateAvgRating(reviews)
 
+ 
   return (
     <div className='tour__card'>
       <Card>
