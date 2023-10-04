90d892007c6396b2c17b226c5efe61261a44c446
soumya <117825720+soumya3969@users.noreply.github.com>
2023-10-04 10:38:30 +0000

    Testimonial Scroll Set-up Done

--- frontend/src/components/Testimonial/Testimonials.jsx
+++ frontend/src/components/Testimonial/Testimonials.jsx
@@ -1,23 +1,102 @@
-import React from 'react'
-import Slider from 'react-slick'
-import ava01 from '../../assets/images/ava-1.jpg'
-import ava02 from '../../assets/images/ava-2.jpg'
-import ava03 from '../../assets/images/ava-3.jpg'
+import React from "react";
+import Slider from "react-slick";
+import ava01 from "../../assets/images/ava-1.jpg";
+import ava02 from "../../assets/images/ava-2.jpg";
+import ava03 from "../../assets/images/ava-3.jpg";
 const Testimonials = () => {
-    return <Slider>
-        <div className="testimonial py-4 px-3">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis rerum hic magni? Sapiente, voluptatum obcaecati consectetur delectus dolorum hic est voluptatibus maxime placeat repellat odit commodi quasi tenetur quae distinctio.</p>
 
-            <div className="d-flex align-items-center gap-4 mt-3">
-                <img src={ava01} className='w-25 h-25 rounded-2' alt="customer_image" />
-                {/* <div>
-                    <h6 className='mb-0 mt-3'>Mr. Desai</h6>
-                    <p> Customer </p>
-                </div> */}
-            </div>
+    const settings ={
+        dots:true,
+        infinite:true,
+        autoplay:true,
+        speed:1000,
+        swipeToSlide:true,
+        autoplaySpeed:2000,
+        slideToShow:3,
+
+        responsive:[
+            {
+                breakpoint:992,
+                settings: {
+                    slideToShow:2,
+                    slideToScroll:1,
+                    infinite:true,
+                    dots:true,
+                },
+            },
+            {
+                breakpoint:576,
+                settings: {
+                  slideToShow:1,
+                  slideToScroll:1,
+                }
+            }
+        ]
+    }
+  return (
+    <Slider {...settings}>
+      <div className="testimonial py-4 px-3">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis rerum
+          hic magni? Sapiente, voluptatum obcaecati consectetur delectus dolorum
+          hic est voluptatibus maxime placeat repellat odit commodi quasi
+          tenetur quae distinctio.
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img
+            src={ava01}
+            className="w-25 h-25 rounded-2"
+            alt="customer_image"
+          />
+          <div>
+            <h6 className="mb-0 mt-3">Mr. Desai</h6>
+            <p> Customer </p>
+          </div>
         </div>
-    </Slider>
+      </div>
+      <div className="testimonial py-4 px-3">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis rerum
+          hic magni? Sapiente, voluptatum obcaecati consectetur delectus dolorum
+          hic est voluptatibus maxime placeat repellat odit commodi quasi
+          tenetur quae distinctio.
+        </p>
 
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img
+            src={ava02}
+            className="w-25 h-25 rounded-2"
+            alt="customer_image"
+          />
+          <div>
+            <h6 className="mb-0 mt-3">Lisa Franklin</h6>
+            <p> Customer </p>
+          </div>
+        </div>
+      </div>
+      <div className="testimonial py-4 px-3">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis rerum
+          hic magni? Sapiente, voluptatum obcaecati consectetur delectus dolorum
+          hic est voluptatibus maxime placeat repellat odit commodi quasi
+          tenetur quae distinctio.
+        </p>
+
+        <div className="d-flex align-items-center gap-4 mt-3">
+          <img
+            src={ava03}
+            className="w-25 h-25 rounded-2"
+            alt="customer_image"
+          />
+          <div>
+            <h6 className="mb-0 mt-3">Tom Disouza</h6>
+            <p> Customer </p>
+          </div>
+        </div>
+      </div>
+    </Slider>
+  );
 };
 
-export default Testimonials
+export default Testimonials;
