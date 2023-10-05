fe24e79849dc68d769166b5247d02c7f49e9c41c
soumya <117825720+soumya3969@users.noreply.github.com>
2023-10-05 18:25:16 +0000

    tour page completed

--- frontend/src/Pages/Tours.jsx
+++ frontend/src/Pages/Tours.jsx
@@ -6,13 +6,13 @@ import "../styles/tour.css";
 import tourData from "../assets/data/tours";
 import TourCard from "../shared/TourCard";
 import SearchBar from "../shared/SearchBar";
-import newsletter from "../shared/newsletter";
+import Newsletter from "../shared/newsletter.jsx";
 
 const Tours = () => {
   const [pageCount, setPageCount] = useState(0);
   const [page, setPage] = useState(0);
   useEffect(() => {
-    const pages = Math.ceil(5 / 8); //!later use backend data count
+    const pages = Math.ceil(5 / 4); //!later use backend data count
     setPageCount(pages);
   }, [page]);
   return (
@@ -37,7 +37,7 @@ const Tours = () => {
             <Col lg="12 ">
               <div className="pagination d-flex align-items-center justify-content-center mt-4 gap-3">
                 {[...Array(pageCount).keys()].map((number) => (
-                  <span key={number} onClick={number}>
+                  <span key={number} onClick={() => setPage(number)} className={page === number ? "active__page":""}>
                     {number + 1}
                   </span>
                 ))}
@@ -46,6 +46,7 @@ const Tours = () => {
           </Row>
         </Container>
       </section>
+      <Newsletter />
     </>
   );
 };
