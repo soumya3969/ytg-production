ae3e5da817f35309d35f1b2dae480dd7438b7a6f
soumya <117825720+soumya3969@users.noreply.github.com>
2023-10-05 17:35:46 +0000

    tour card started

--- frontend/src/Pages/Tours.jsx
+++ frontend/src/Pages/Tours.jsx
@@ -1,5 +1,39 @@
 import React from "react";
-const Tours =()=>{
-    return <div>Tours</div>;
+import CommonSection from "../shared/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+
+import "../styles/tour.css";
+import tourData from "../assets/data/tours";
+import TourCard from "../shared/TourCard";
+import SearchBar from "../shared/SearchBar";
+import newsletter from "../shared/newsletter";
+
+const Tours = () => {
+  return (
+    <>
+      <CommonSection title={"All Tours"} />
+      <section>
+        <Container>
+          <Row>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+      <section className="pt-0">
+        <Container>
+          <Row>
+            {tourData?.map((tour) => (
+              <Col lg="3" className="mb-4" key={tour.id}>
+                <TourCard tour={tour} />
+              </Col>
+            ))}
+
+
+            <Col lg='12 '></Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 export default Tours;
