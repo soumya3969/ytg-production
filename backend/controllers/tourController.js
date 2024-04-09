ef1b3609ccb14b41c1f0042b7c6104e7d4aa5eec
soumya <117825720+soumya3969@users.noreply.github.com>
2024-04-09 17:28:14 +0000

    database requests setup

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -6,13 +6,11 @@ export const createTour = async (req, res) => {
 
   try {
     const savedTour = await newTour.save();
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Successfully created",
-        data: savedTour,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Successfully created",
+      data: savedTour
+    });
   } catch (err) {
     res
       .status(500)
@@ -20,67 +18,87 @@ export const createTour = async (req, res) => {
   }
 };
 
-
 // *update tours
-export const updateTour = async(req,res)=>{
-    const id = req.params.id
-    try {
-        const updatedTour=await Tour.findByIdAndUpdate(id, {
-            $set:req.body
-        },{new:true})
-
-        res
-        .status(200)
-        .json({
-          success: true,
-          message: "Successfully updated",
-          data: updatedTour,
-        });
-
-    } catch (err) {
-        res
-        .status(500)
-        .json({
-          success: false,
-          message: "failed to update",
-        });
-    }
-}
-// *Delete tours
-export const deleteTour = async(req,res)=>{
-  const id = req.params.id
+export const updateTour = async (req, res) => {
+  const id = req.params.id;
   try {
-      await Tour.findByIdAndDelete(id)
+    const updatedTour = await Tour.findByIdAndUpdate(
+      id,
+      {
+        $set: req.body
+      },
+      { new: true }
+    );
 
-      res
-      .status(200)
-      .json({
-        success: true,
-        message: "Successfully deleted",
-      });
-
+    res.status(200).json({
+      success: true,
+      message: "Successfully updated",
+      data: updatedTour
+    });
   } catch (err) {
-      res
-      .status(500)
-      .json({
-        success: false,
-        message: "failed to delete",
-      });
+    res.status(500).json({
+      success: false,
+      message: "failed to update"
+    });
   }
-}
+};
+// *Delete tours
+export const deleteTour = async (req, res) => {
+  const id = req.params.id;
+  try {
+    await Tour.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully deleted"
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: "failed to delete"
+    });
+  }
+};
 // *getSingle tours
-export const getSingleTour = async(req,res)=>{
-    try {
-        
-    } catch (err) {
-        
-    }
-}
+export const getSingleTour = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const tour = await Tour.findById(id);
+
+    res.status(200).json({
+      success: true,
+      message: "Successfully Fetched",
+      data: tour
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: "Not Found"
+    });
+  }
+};
 // *getAll tours
-export const getAllTour = async(req,res)=>{
-    try {
-        
-    } catch (err) {
-        
-    }
-}
+export const getAllTour = async (req, res) => {
+  // * For Pagination
+  const page = parseInt(req.query.page);
+
+  // console.log(page);
+
+  try {
+    const tours = await Tour.find({})
+      .skip(page * 8)
+      .limit(8);
+
+    res.status(200).json({
+      success: true,
+      count:tours.length,
+      message: "Successfully Fetched",
+      data: tours
+    });
+  } catch (err) {
+    res.status(404).json({
+      success: false,
+      message: "Not Found"
+    });
+  }
+};
