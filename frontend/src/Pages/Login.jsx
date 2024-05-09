48e20c3772cbbe3c24c38aa32c6bd17fbfa62770
soumya3969 <srmhnt2001@gmail.com>
2024-05-10 02:46:40 +0530

    P4-34:43login page start

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,55 +1,75 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import "../styles/login.css";
 import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
 import { Link } from "react-router-dom";
-import loginImg from '../assets/images/login.png'
-import userIcon from '../assets/images/user.png'
+import loginImg from "../assets/images/login.png";
+import userIcon from "../assets/images/user.png";
+
+import { AuthContext } from "../context/AuthContext";
+import { BASE_URL } from "../utils/config";
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    email: undefined,
+    password: undefined
+  });
+  const handleChange = (e) => {
+    setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
 
-    const [credentials, setCredentials] = useState({
-       email:undefined,
-       password:undefined
-      });
-    const handleChange = (e) => {
-        setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
-      };
-      
-
-      const handleClick=e=>{
-        e.preventDefault();
-      }
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
   return (
     <section>
-    <Container>
+      <Container>
         <Row>
-            <Col lg='8' className="m-auto">
-                <div className="login__container d-flex justify-content-between">
-                    <div className="login__img">
-                        <img src={loginImg} alt="" />
-                    </div>
-                    <div className="login__form">
-                        <div className="user">
-                            <img src={userIcon} alt="" />
-                        </div>
-                        <h2>Login</h2>
-                        <Form onSubmit={handleClick}>
-                            <FormGroup>
-                                <input type="email" placeholder="Email" required id="email" onChange={handleChange} />
-                            </FormGroup>
-                            <FormGroup>
-                                <input type="password" placeholder="Password" required id="password" onChange={handleChange} />
-                            </FormGroup>
-                            <Button className="btn secondary__btn auth__btn" type='submit'>Login</Button>
-                        </Form>
-                        <p>Don't have an account? <Link to='/register'>Create Here.</Link></p>
-                    </div>
+          <Col lg="8" className="m-auto">
+            <div className="login__container d-flex justify-content-between">
+              <div className="login__img">
+                <img src={loginImg} alt="" />
+              </div>
+              <div className="login__form">
+                <div className="user">
+                  <img src={userIcon} alt="" />
                 </div>
-            </Col>
+                <h2>Login</h2>
+                <Form onSubmit={handleClick}>
+                  <FormGroup>
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      required
+                      id="email"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <FormGroup>
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      required
+                      id="password"
+                      onChange={handleChange}
+                    />
+                  </FormGroup>
+                  <Button
+                    className="btn secondary__btn auth__btn"
+                    type="submit"
+                  >
+                    Login
+                  </Button>
+                </Form>
+                <p>
+                  Don't have an account?{" "}
+                  <Link to="/register">Create Here.</Link>
+                </p>
+              </div>
+            </div>
+          </Col>
         </Row>
-    </Container>
-</section>
-
+      </Container>
+    </section>
   );
 };
 export default Login;
