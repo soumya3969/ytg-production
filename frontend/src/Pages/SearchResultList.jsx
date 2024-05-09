60e6c1a11606752979ffe798b4f15905e0a72fbf
soumya3969 <srmhnt2001@gmail.com>
2024-05-09 14:35:58 +0530

    searchbar integration completed

--- frontend/src/Pages/SearchResultList.jsx
+++ frontend/src/Pages/SearchResultList.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
 
 import CommonSection from "./../shared/CommonSection";
-import { Container, Row } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 import { useLocation } from "react-router-dom";
 import TourCard from "../shared/TourCard";
+import Newsletter from "./../shared/newsletter";
 
 const SearchResultList = () => {
   const location = useLocation();
@@ -16,12 +17,22 @@ const SearchResultList = () => {
   return (
     <>
       <CommonSection title={"Tour Search Result"} />
-
       <section>
         <Container>
-          <Row></Row>
+          <Row>
+            {data.length === 0 ? (
+              <h4 className="text-center">No Tour Found</h4>
+            ) : (
+              data?.map((tour) => (
+                <Col lg="3" className="mb-4" key={tour._id}>
+                  <TourCard tour={tour} />
+                </Col>
+              ))
+            )}
+          </Row>
         </Container>
       </section>
+      <Newsletter />
     </>
   );
 };
