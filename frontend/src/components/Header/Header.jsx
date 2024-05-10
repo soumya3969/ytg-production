658eda62099005514d9fb9aeb4175a8b621cade0
soumya3969 <srmhnt2001@gmail.com>
2024-05-10 14:17:21 +0530

    header integration completed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,85 +1,117 @@
-import React,{useRef,useEffect} from "react";
-import { Container, Row, Button } from 'reactstrap'
-import { NavLink, Link } from 'react-router-dom'
+import React, { useRef, useEffect, useContext } from "react";
+import { Container, Row, Button } from "reactstrap";
+import { NavLink, Link, useNavigate } from "react-router-dom";
 
-import logo from '../../assets/images/logo.png'
-import './header.css'
+import logo from "../../assets/images/logo.png";
+import "./header.css";
+
+import { AuthContext } from "../../context/AuthContext";
 
 const nav__links = [
-    {
-        path: '/home',
-        display: 'Home'
-    },
-    {
-        path: '/about',
-        display: 'About'
-    },
-    {
-        path: '/tours',
-        display: 'Tours'
-    }
-]
+  {
+    path: "/home",
+    display: "Home"
+  },
+  {
+    path: "/about",
+    display: "About"
+  },
+  {
+    path: "/tours",
+    display: "Tours"
+  }
+];
 
 const Header = () => {
+  const headerRef = useRef(null);
+  const navigate = useNavigate();
+  const { user, dispatch } = useContext(AuthContext);
 
-    const headerRef = useRef(null)
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+  };
 
-    const stickyHeaderFunc = () =>{
-        window.addEventListener('scroll',()=>{
-            if(document.body.scrollTop >80 || document.documentElement.scrollTop>80){
-                headerRef.current.classList.add('sticky__header');
-            }else{
-                headerRef.current.classList.remove('sticky__header');
+  const stickyHeaderFunc = () => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    });
+  };
 
-            }
-        })
-    }
+  useEffect(() => {
+    stickyHeaderFunc();
 
-    useEffect(()=>{
-        stickyHeaderFunc()
+    return window.removeEventListener("scroll", stickyHeaderFunc);
+  });
+  return (
+    <header className="header" ref={headerRef}>
+      <Container>
+        <Row>
+          <div className="nav__wrapper d-flex align-items-center justify-content-between">
+            {/* =====Logo section ====== */}
 
-        return window.removeEventListener('scroll', stickyHeaderFunc)
-    })
-    return (
-        <header className="header" ref={headerRef}>
-            <Container>
-                <Row>
-                    <div className="nav__wrapper d-flex align-items-center justify-content-between">
+            <div className="logo">
+              <img src={logo} alt="" />
+            </div>
+            {/* =====Logo section end ====== */}
 
-                        {/* =====Logo section ====== */}
+            {/* =====Menu section start ====== */}
+            <div className="navigation">
+              <ul className="menu d-flex align-items-center gap-5">
+                {nav__links.map((item, index) => (
+                  <li className="nav__item" key={index}>
+                    <NavLink
+                      to={item.path}
+                      className={(navClass) =>
+                        navClass.isActive ? "active__link" : ""
+                      }
+                    >
+                      {item.display}
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* =====Menu section end ====== */}
 
-                        <div className="logo">
-                            <img src={logo} alt="" />
-                        </div>
-                        {/* =====Logo section end ====== */}
+            <div className="nav__right d-flex align-items-center gap-4">
+              <div className="nab__btns d-flex align-items-center gap-4">
+                {user ? (
+                  <>
+                    <h5 className="mb-0">{user.username}</h5>
 
-                        {/* =====Menu section start ====== */}
-                        <div className="navigation">
-                            <ul className="menu d-flex align-items-center gap-5">
-                                {nav__links.map((item, index) => (
-                                    <li className="nav__item" key={index}>
-                                        <NavLink to={item.path} className={navClass=>navClass.isActive ? 'active__link':""}>{item.display}</NavLink>
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                        {/* =====Menu section end ====== */}
+                    <Button className="btn btn-dark" onClick={logout}>
+                      Logout
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Button className="btn secondary__btn">
+                      <Link to="/login">Login</Link>
+                    </Button>
+                    <Button className="btn primary__btn">
+                      <Link to="/register">Register</Link>
+                    </Button>
+                  </>
+                )}
+              </div>
 
-                        <div className="nav__right d-flex align-items-center gap-4">
-                            <div className="nab__btns d-flex align-items-center gap-4">
-                                <Button className="btn secondary__btn"><Link to='/login'>Login</Link></Button>
-                                <Button className="btn primary__btn"><Link to='/register'>Register</Link></Button>
-                            </div>
-
-                            <span className="mobile__menu">
-                                <i class="ri-menu-line"></i>
-                            </span>
-                        </div>
-                    </div>
-                </Row>
-            </Container>
-        </header>
-    );
+              <span className="mobile__menu">
+                <i class="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Row>
+      </Container>
+    </header>
+  );
 };
 
 export default Header;
