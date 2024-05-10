4e3636c11e80120963075520763c0dfdea76ae7a
soumya3969 <srmhnt2001@gmail.com>
2024-05-10 12:55:23 +0530

    login page integration started

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "../styles/login.css";
 import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import loginImg from "../assets/images/login.png";
 import userIcon from "../assets/images/user.png";
 
